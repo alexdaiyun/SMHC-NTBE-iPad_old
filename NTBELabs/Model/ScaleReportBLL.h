4c4fc86160fbb509826bfd7b9fbf2878ae5e0885
alexdaiyun <daiyunstar@gmail.com>
2013-05-03 10:27:23 +0800

    更新

---
+++ NTBELabs/Model/ScaleReportBLL.h
@@ -0,0 +1,14 @@
+//
+//  ScaleReportBLL.h
+//  NTBELabs
+//
+//  Created by dai yun on 13-5-3.
+//  Copyright (c) 2013年 alexday. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+/* 量表报告逻辑处理 */
+@interface ScaleReportBLL : NSObject
+
+@end
---
+++ NTBELabs/Model/ScaleReportBLL.m
@@ -0,0 +1,13 @@
+//
+//  ScaleReportBLL.m
+//  NTBELabs
+//
+//  Created by dai yun on 13-5-3.
+//  Copyright (c) 2013年 alexday. All rights reserved.
+//
+
+#import "ScaleReportBLL.h"
+
+@implementation ScaleReportBLL
+
+@end
