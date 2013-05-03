1ece4a65f22fff6553503adb1d557673f028a052
alexdaiyun <daiyunstar@gmail.com>
2013-05-03 10:36:43 +0800

    111

--- NTBELabs/util/Constants.h
+++ NTBELabs/util/Constants.h
@@ -14,3 +14,13 @@
 
 
 #endif
+
+typedef enum {
+    ScaleLevelMMSE, //简明精神状态检查
+    ScaleLevelMOCA, //蒙特利尔认知评估
+    ScaleLevelNTB,  //成套神经心理测验
+    ScaleLevelSAS,  //焦虑自评量表
+    ScaleLevelGDS,  //老年抑郁量表
+    ScaleLevelLES,  //生活事件量表
+    ScaleLevelSSRS  //社会支持评定量表
+} ScaleLevelTag;
